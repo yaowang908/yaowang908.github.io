c6781e1902f613e6e5944639bbfb2a80d7af28d0
Yao Wang <yao422321065@yahoo.com>
2016-04-17 19:35:05 -0400

    update about me

--- scripts.js
+++ scripts.js
@@ -4,9 +4,11 @@ window.onload = function(){
 	var outstripe = document.getElementsByClassName("outstripe");
 	var ghostbutton = document.getElementsByClassName("ghostbutton");
 	var ghostbutton_back = document.getElementsByClassName("ghostbutton_back");
+	var aboutme = document.getElementById("aboutme");
+	var t = document.getElementById("t");
+	var closeicon = document.getElementById("close");
 	
 	for(var i=0;i<container.length;i++){
-	
 	ghostbutton[i].addEventListener('click',function(){
 		this.parentNode.style.marginLeft= "-90%";
 		this.parentNode.nextSibling.nextSibling.style.marginLeft= "10%";
@@ -14,20 +16,29 @@ window.onload = function(){
 		this.nextSibling.nextSibling.style.display = "block";
 		});
 	}
-	for(var i=0;i<container.length;i++){
-	container[i].addEventListener('mouseleave',function(){
+	
+	for(var n=0;n<container.length;n++){
+	container[n].addEventListener('mouseleave',function(){
 		this.children[0].style.marginLeft= "0";
 		this.children[1].style.marginLeft= "100%";
 		this.children[0].children[0].style.display = "block";
 		this.children[0].children[1].style.display = "none";
 		});
 	}
-	for(var i=0;i<container.length;i++){
-	ghostbutton_back[i].addEventListener('click',function(){
+	
+	for(var m=0;m<container.length;m++){
+	ghostbutton_back[m].addEventListener('click',function(){
 		this.parentNode.style.marginLeft= "0";
 		this.parentNode.nextSibling.nextSibling.style.marginLeft= "100%";
 		this.parentNode.children[0].style.display = "block";
 		this.style.display = "none";
 		});
 	}
-	};
+	aboutme.addEventListener('click',function(){
+		t.style.display="block";
+		});
+	closeicon.addEventListener('click',function(){
+		t.style.display="none";
+		});
+	
+};
