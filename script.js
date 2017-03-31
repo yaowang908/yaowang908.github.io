2006077a7a10de1d2625f1be71f798d4c62b15d0
Yao Wang <yao422321065@yahoo.com>
2017-03-31 16:00:48 -0400

    angular js

---
+++ script.js
@@ -0,0 +1,33 @@
+var appFree = angular.module('free',[]);
+appFree.controller('freeImageCtrl',function($scope){
+    $scope.freeImageSites=[
+        {url :"http://pixabay.com",name:"Pixabay"},
+        {url :"http://www.sitebuilderreport.com/stock-up",name:"Stock up"},
+        {url :"https://www.pixelsquid.com/",name:"Pixel Squid"},
+        {url :"http://www.iconfont.cn/",name:"Icon Font by Alibaba"},
+        {url :"https://unsplash.com/",name:"Unsplash"},
+        {url :"http://www.gratisography.com/",name:"Gratisography"},
+        {url :"http://www.lifeofpix.com/",name:"Life of pix"},
+        {url :"http://publicdomainarchive.com/",name:"Public domain archive"},
+        {url :"http://jaymantri.com/",name:"Jaymantri"},
+        {url :"http://littlevisuals.co",name:"Little visuals"},
+        {url :"http://nos.twnsnd.co/",name:"New old stock"},
+        {url :"https://foodiesfeed.com/",name:"Foodies feed"},
+        {url :"http://raumrot.com/photo-set-landing/",name:"Raumrot"},
+        {url :"http://kaboompics.com/",name:"Kaboompics"},
+        {url :"http://www.splitshire.com/",name:"Split shire"},
+        {url :"http://magdeleine.co/browse/",name:"Magdeleine"},
+        {url :"http://picography.co/",name:"Picography"},
+    ];
+});
+appFree.controller('freeFontCtrl',function($scope){
+    $scope.freeFontSites=[
+        {url:"https://www.fontsquirrel.com/",name:"Free font"},
+    ];
+});
+appFree.controller('freeSearchCtrl',function($scope){
+    $scope.freeSearchSites=[
+        {url:"https://search.creativecommons.org/",name:"CC Search"},
+    ];
+});
+
