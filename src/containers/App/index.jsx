504661f14b37a42faa363b8a8e0eb7b9f2c82048
Yao Wang <yao422321065@yahoo.com>
2018-12-27 17:12:16 -0500

    init canvas

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const BackgroundCanvas = styled.canvas`
+    width: 100%;
+    height:100%;
+    margin: 0;
+    padding：0；
+`;
 
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.updateCanvas = this.updateCanvas.bind(this);
         this.state={
 
         }
     }
 
+    updateCanvas() {
+        const canvas = this.refs.canvas;
+        if(canvas.getContext) {
+            const ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = 'rgb(200, 0, 0)';
+            for(let i = 20; i>0; i=i-3) {
+                ctx.fillRect(i,i,1,1);
+            }
+            // TODO: canvas is blurry
+        } else {
+            alert ('Canvas is not supported!');
+        }
+    }
+
+    componentDidMount() {
+        this.updateCanvas();
+    }
+
+    componentDidUpdate() {
+        this.updateCanvas();
+    }
+
+
     render(){
         return (
-            <h1>Here is new design!</h1>
+            <BackgroundCanvas ref="canvas"></BackgroundCanvas>
         );
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./containers/App/index.jsx";
+import "../assets/app.scss";
 
 ReactDOM.render(
     <App />,
