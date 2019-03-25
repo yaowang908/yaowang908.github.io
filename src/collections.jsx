4f420917011537e5a0cd864b0c2b3146b696e4e2
Yao Wang <yao422321065@yahoo.com>
2019-03-25 17:18:22 -0400

    clear up folders

--- src/collections.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Collections from "./containers/Collections/index.jsx";
-import "../assets/collections.scss";
-
-ReactDOM.render(
-    <Collections />,
-    document.getElementById("collections_root")
-);
