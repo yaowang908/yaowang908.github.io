dbfa1cc08702979f743a7415f2c049d8af6c04d7
Yao Wang <yao422321065@yahoo.com>
2019-03-24 20:29:15 -0400

    add projects

--- src/App/projectsData.jsx
+++ src/App/projectsData.jsx
@@ -18,16 +18,16 @@ const projectsData = [
         color: 'rgb(232,298,97)',
     },
     {
-        name: 'name1',
-        link: 'link1',
-        imgUrl: 'imgUrl1',
-        color: 'color1',
+        name: '2048',
+        link: 'https://www.yaow.me/2048/',
+        imgUrl: '../../assets/2048.png',
+        color: 'rgb(232,298,97)',
     },
     {
-        name: 'name1',
-        link: 'link1',
-        imgUrl: 'imgUrl1',
-        color: 'color1',
+        name: 'Mind-mapping',
+        link: 'https://www.yaow.me/CE-Mindmap/dist/',
+        imgUrl: '../../assets/mind-mapping.png',
+        color: 'rgb(232,298,97)',
     },
 ];
 
